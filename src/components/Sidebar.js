b3d6ac32a79cb3314054c4c3d5f0c643f61b250c
fahimrimon <fahimrimon21@gmail.com>
2023-03-18 09:04:28 -1200

    product show

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const {isOpen, handleClose} = useContext(SidebarContext);
+  return <div className={`${isOpen ? "right-0 " : "-right-full"} h-full w-full top-0 bg-white fixed shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
+    Sidebar
+    </div>;
 };
 
 export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useContext } from 'react';
+import Product from '../components/Product';
 import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
@@ -12,9 +13,18 @@ const Home = () => {
       item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
-  console.log(filterProducts);
   return <div>
-    Homepage
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
+          {filterProducts?.map((product) =>{
+            return(
+              <Product product={product} key={product.id}></Product>
+            )
+          })}
+        </div>
+      </div>
+    </section>
   </div>;
 };
 
